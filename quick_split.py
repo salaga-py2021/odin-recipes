1381d9847212dd0a1bdfe784460a65b79c9d8844
salaga-py2021 <pxg200018@utdallas.edu>
2022-05-22 19:11:20 -0500

    Add gulab_jamun.html & related information.
    The gulab_jamun file is structured similarly to the other files.
    Used the quick_split file to convert ingredients into html code.
    Uploaded new images for the pictures on the website.

---
+++ quick_split.py
@@ -0,0 +1,27 @@
+data = """
+2 cups water
+
+2 cups white sugar
+
+½ teaspoon ground cardamom
+
+2 drops rose water (Optional)
+
+1 pinch saffron (Optional)
+
+½ cup instant dry milk powder (such as Carnation®)
+
+2 tablespoons all-purpose flour
+
+¼ teaspoon baking soda
+
+1 tablespoon unsalted butter (such as Land O'Lakes®)
+
+2 tablespoons plain yogurt
+
+2 cups vegetable oil for frying
+"""
+
+for i in data.split('\n'):
+    if len(i) > 1:
+        print("<li>",i,"</li>") 
